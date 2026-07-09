307ab0ab66fb84a0728bae3426a1ea784b6c2bab
Elyon <elyon@MacBook-Air-de-Elyon.local>
2026-07-09 13:04:14 -0300

    fix: resolve next.config and eslint config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Ignorando erros de ESLint (como variáveis 'any' antigas) para não travar o deploy no Vercel
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
